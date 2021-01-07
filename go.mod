3464c668ef15c46aa90123d7eb80d8e0603e6605
yenalgogebakan <yenal.gogebakan@cybersoft.com.tr>
2021-01-07 13:34:08 +0300

    gin started

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ replace logging => ./logging
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/sirupsen/logrus v1.7.0 // indirect
 	logging v0.0.0-00010101000000-000000000000 // indirect
 )
--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"logging"
+	"github.com/gin-gonic/gin"
 )
 // Global variables
 var mainlog, infolog *logrus.Logger
 
+type Person struct {
+	Name    string `form:"name" json:"name"`
+	Address string `form:"address" json:"address"`
+}
 func main () {
 	// Log setup
 	if err := logging.CreateDefaultLogs (); err != nil {
@@ -32,4 +37,24 @@ func main () {
 		"func": "main ()",
 	}).Info("Expenses is starting !!!")
 
+	route := gin.Default()
+	route.GET("/testing", startPage)
+	route.Run(":8085")
+}
+
+func startPage(c *gin.Context) {
+	var person Person
+	if c.Bind(&person) == nil {
+		fmt.Println("====== Bind By Query String ======")
+		fmt.Println(person.Name)
+		fmt.Println(person.Address)
+	}
+	if c.BindJSON(&person) == nil {
+		fmt.Println("====== Bind By JSON ======")
+		fmt.Println(person.Name)
+		fmt.Println(person.Address)
+	}
+
+//	c.String(200, "Success")
+c.JSON (200, gin.H {"message":"OK", "Cevap": 2})
 }
